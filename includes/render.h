ea46a0a619e37448783545579009d8fac6a3af4e
AnthonyDcodes <dvoracekantonin@icloud.com>
2026-06-17 13:05:42 +0200

    create new t_vector struct for vector calculations

--- includes/render.h
+++ includes/render.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/15 12:12:21 by advorace          #+#    #+#             */
-/*   Updated: 2026/06/17 10:28:13 by advorace         ###   ########.fr       */
+/*   Updated: 2026/06/17 13:04:48 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,31 @@
 
 #include "cub3d.h"
 
+typedef struct s_vector
+{
+	double		camera_x;
+	double		delta_dist_x;
+	double		delta_dist_y;
+	double		ray_dir_x;
+	double		ray_dir_y;
+	int			map_x;
+	int			map_y;
+	double		side_dist_x;
+	double		side_dist_y;
+	int			step_x;
+	int			step_y;
+	int			hit;
+	int			side;
+}	t_vector;
+
 void	clean_up(t_scene *scene, int exit_status);
 int		create_trgb(int t, int r, int g, int b);
 void	my_mlx_pixel_put(t_scene *scene, int x, int y, int color);
 void	put_scene_pixel(t_scene *scene, int x1, int y1);
 int		render_scene(void *param);
+
+// Vector calculations
+void	assign_ray_dirs(t_vector *vector, t_player *player);
+void	assign_delta_dists(t_vector *vector);
+void	assign_map_pos(t_vector *vector, t_player *player);
+void	assign_step_side_dist(t_vector *vector, t_player *player);
---
+++ src/render/vector_calc.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_calc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/17 12:50:56 by advorace          #+#    #+#             */
+/*   Updated: 2026/06/17 13:04:30 by advorace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+#include "render.h"
+
+void assign_ray_dirs(t_vector *vector, t_player *player)
+{
+	double	p_dir_x;
+	double	p_dir_y;
+	double	p_plane_x;
+	double	p_plane_y;
+	
+	p_dir_x = player->dir_x;
+	p_dir_y = player->dir_y;
+	p_plane_x = player->plane_x;
+	p_plane_x = player->plane_y;
+	vector->ray_dir_x = p_dir_x + p_plane_x * vector->camera_x;
+	vector->ray_dir_y = p_dir_y + p_plane_y * vector->camera_x;
+}
+
+void assign_delta_dists(t_vector *vector)
+{
+	// For DDA only ratios not the absolute values are needed
+	vector->delta_dist_x = fabs(1 / vector->ray_dir_x);
+	vector->delta_dist_y = fabs(1 / vector->ray_dir_y);
+}
+
+void assign_map_pos(t_vector *vector, t_player *player)
+{
+	// Get current posstion on the map
+	vector->map_x = player->pos_x;
+	vector->map_y = player->pos_y;
+}
+
+void assign_step_side_dist(t_vector *vector, t_player *player)
+{
+	if (vector->ray_dir_x < 0)
+	{
+		vector->step_x = -1;
+		vector->side_dist_x = (player->pos_x - vector->map_x) * vector->delta_dist_x;
+	}
+	else
+	{
+		vector->step_x = 1;
+		vector->side_dist_x = (player->pos_x - vector->map_x + 1) * vector->delta_dist_x;
+	}
+	if (vector->ray_dir_y < 0)
+	{
+		vector->step_y = -1;
+		vector->side_dist_y = (player->pos_y - vector->map_y) * vector->delta_dist_y;
+	}
+	else
+	{
+		vector->step_y = 1;
+		vector->side_dist_y = (player->pos_y - vector->map_y + 1) * vector->delta_dist_y;
+	}
+}
